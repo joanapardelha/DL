ad01f843e92cea993b7536e559a480e9a302f7b3
joanapardelha <joanapms01@gmail.com>
2023-04-06 14:06:23 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import datasets
-#from tensorflow.keras.preprocessing import image_dataset_from_directory
+
 from tensorflow.keras import Sequential, Model, layers, initializers, regularizers, optimizers, metrics
 
 def preprocessing_data(path, main_folder, testing_folder):
